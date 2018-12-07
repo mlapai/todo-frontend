bb752859f76f3743000fbd36746c7eef7ddf276d
Mladen Paic <mladenpaic@Mladens-MacBook-Pro.local>
2018-12-07 14:12:44 +0100

    added todo patch

--- src/app/todo-detail/todo-detail.component.ts
+++ src/app/todo-detail/todo-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {MessageService} from '../message.service';
 
 @Component({
   selector: 'app-todo-detail',
@@ -7,7 +9,18 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TodoDetailComponent implements OnInit {
   @Input() todo;
-  constructor() { }
+  private http;
+  private messageService;
+  constructor(http: HttpClient, messageService: MessageService) {
+    this.http = http;
+    this.messageService = messageService;
+  }
+  onSave(todo) {
+    this.http.patch('http://myapp.test/api/todos/' + todo.id, todo)
+      .subscribe(() => {
+        this.messageService.add('Todo with id ' + todo.id + ' updated');
+      });
+  }
 
   ngOnInit() {
   }
